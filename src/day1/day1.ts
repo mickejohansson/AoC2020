5215a2ef8007fb7b8e7dd66d6149cd138a36c76f
Mikael Johansson <mikael@mikaeljohansson.org>
2020-11-30 20:17:09 +0100

    First day 2019 as example

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,5 +1,7 @@
-const calculate = () => {
-  return 3
+const calculate = (weights: Array<number>) => {
+  return weights.reduce((accumulator, currentValue) => {
+    return accumulator + Math.floor(currentValue / 3) - 2
+  }, 0)
 }
 
 export default calculate
