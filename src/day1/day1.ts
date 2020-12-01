38e04f105c1913397b4411e4f9175231c7f64abb
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-01 08:02:24 +0100

    Solve part 2

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,4 +1,4 @@
-const findProduct = (entries: Array<number>) => {
+const find2Product = (entries: Array<number>) => {
   let result: number = undefined
   entries.forEach((entry) => {
     if (entries.includes(2020 - entry)) {
@@ -9,4 +9,19 @@ const findProduct = (entries: Array<number>) => {
   return result
 }
 
-export default findProduct
+const find3Product = (entries: Array<number>) => {
+  for (const i of entries) {
+    for (const j of entries) {
+      const partSum = i + j
+      if (partSum < 2020) {
+        if (entries.includes(2020 - partSum)) {
+          return (2020 - partSum) * i * j
+        }
+      }
+    }
+  }
+
+  return undefined
+}
+
+export default { find2Product, find3Product }
