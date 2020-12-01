b8f7fe238b487ac4772118acb231305a601b5dfe
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-01 23:02:28 +0100

    Use fileReader in tests

--- src/util/fileReader.ts
+++ src/util/fileReader.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs'
 
 const readArray = (path: string) => {
-  return readFileSync(path, { encoding: 'utf8' }).split('\n')
+  return readFileSync(path, { encoding: 'utf8' }).trim().split('\n')
 }
 
 export default { readArray }
