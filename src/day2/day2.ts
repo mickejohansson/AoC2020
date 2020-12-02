7fd31573493584355e961b129f8409c07d951cff
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-02 06:29:00 +0100

    Create entry object

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,5 +1,15 @@
+const entry = (line: string) => {
+  const lineParts = line.split(' ')
+  const range = lineParts[0].split('-').map((l) => parseInt(l))
+  return {
+    range,
+    letter: lineParts[1].replace(':', ''),
+    password: lineParts[2]
+  }
+}
+
 const nbrValidPasswords = (lines: Array<string>) => {
-  return 3
+  return lines.map((line) => entry(line))
 }
 
 export default { nbrValidPasswords }
