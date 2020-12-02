4c33391e1a36f4c23289b0434939dad2551ba117
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-02 06:38:27 +0100

    Part 1 done

--- src/day2/day2.test.ts
+++ src/day2/day2.test.ts
@@ -5,4 +5,7 @@ test('', () => {
   expect(
     day2.nbrValidPasswords(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'])
   ).toBe(2)
+  expect(
+    day2.nbrValidPasswords(fileReader.readStringArray(__dirname + '/input.txt'))
+  ).toBe(439)
 })
--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -9,7 +9,19 @@ const entry = (line: string) => {
 }
 
 const nbrValidPasswords = (lines: Array<string>) => {
-  return lines.map((line) => entry(line))
+  const entries = lines.map((line) => entry(line))
+
+  let nbrCorrect = 0
+  entries.forEach((entry) => {
+    const nbrOccurencies = entry.password
+      .split('')
+      .filter((c) => c === entry.letter).length
+    if (nbrOccurencies >= entry.range[0] && nbrOccurencies <= entry.range[1]) {
+      nbrCorrect += 1
+    }
+  })
+
+  return nbrCorrect
 }
 
 export default { nbrValidPasswords }
