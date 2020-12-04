d2e40f60a02aee59f2ea50878ccb68565aefb74b
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-04 20:12:30 +0100

    Solve part 2

--- src/day4/day4.test.ts
+++ src/day4/day4.test.ts
@@ -1,6 +1,52 @@
 import day4 from './day4'
-
 test('it returns the number of valid inputs', () => {
   expect(day4.nbrValidPassports(__dirname + '/testInput.txt')).toBe(2)
   expect(day4.nbrValidPassports(__dirname + '/input.txt')).toBe(222)
+})
+
+test('it senses valid fields', () => {
+  expect(day4.isFieldValid(['byr', '2002'])).toBe(true)
+  expect(day4.isFieldValid(['byr', '2003'])).toBe(false)
+  expect(day4.isFieldValid(['byr', '202'])).toBe(false)
+  expect(day4.isFieldValid(['byr', '2022'])).toBe(false)
+  expect(day4.isFieldValid(['byr', '1900'])).toBe(false)
+
+  expect(day4.isFieldValid(['iyr', '1900'])).toBe(false)
+  expect(day4.isFieldValid(['iyr', '2011'])).toBe(true)
+  expect(day4.isFieldValid(['iyr', '205'])).toBe(false)
+
+  expect(day4.isFieldValid(['eyr', '2020'])).toBe(true)
+  expect(day4.isFieldValid(['eyr', '2011'])).toBe(false)
+
+  expect(day4.isFieldValid(['hgt', '60in'])).toBe(true)
+  expect(day4.isFieldValid(['hgt', '190cm'])).toBe(true)
+  expect(day4.isFieldValid(['hgt', '190in'])).toBe(false)
+  expect(day4.isFieldValid(['hgt', '190'])).toBe(false)
+
+  expect(day4.isFieldValid(['hcl', '#123abc'])).toBe(true)
+  expect(day4.isFieldValid(['hcl', '123abc'])).toBe(false)
+  expect(day4.isFieldValid(['hcl', '#123abz'])).toBe(false)
+
+  expect(day4.isFieldValid(['ecl', 'brn'])).toBe(true)
+  expect(day4.isFieldValid(['ecl', 'wat'])).toBe(false)
+
+  expect(day4.isFieldValid(['pid', '000043212'])).toBe(true)
+  expect(day4.isFieldValid(['pid', '43212'])).toBe(false)
+  expect(day4.isFieldValid(['pid', '43212343434343'])).toBe(false)
+})
+
+test('it finds all the invalid passports', () => {
+  expect(
+    day4.nbrValidPassports(__dirname + '/invalidPassports.txt', true)
+  ).toBe(0)
+})
+
+test('it finds all the valid passports', () => {
+  expect(day4.nbrValidPassports(__dirname + '/validPassports.txt', true)).toBe(
+    4
+  )
+})
+
+test('it finds the number of valid passports', () => {
+  expect(day4.nbrValidPassports(__dirname + '/input.txt', true)).toBe(140)
 })
--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -1,13 +1,48 @@
 import fileReader from '../util/fileReader'
 
-const nbrValidPassports = (path: string) => {
-  const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+const isFieldValid = (field: Array<string>): boolean => {
+  if (field[0] === 'byr') {
+    const year = parseInt(field[1])
+    return field[1].length === 4 && year >= 1920 && year <= 2002
+  } else if (field[0] === 'iyr') {
+    const year = parseInt(field[1])
+    return field[1].length === 4 && year >= 2010 && year <= 2020
+  } else if (field[0] === 'eyr') {
+    const year = parseInt(field[1])
+    return field[1].length === 4 && year >= 2020 && year <= 2030
+  } else if (field[0] === 'hgt') {
+    const unit = field[1].slice(field[1].length - 2)
+    const value = parseInt(field[1].slice(0, field[1].length - 2))
+    return (
+      (unit === 'cm' && value >= 150 && value <= 193) ||
+      (unit === 'in' && value >= 59 && value <= 76)
+    )
+  } else if (field[0] === 'hcl') {
+    return /^#[0-9a-f]{6}$/g.test(field[1])
+  } else if (field[0] === 'ecl') {
+    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(field[1])
+  } else if (field[0] === 'pid') {
+    return /^\d{9}$/g.test(field[1])
+  } else if (field[0] === 'cid') {
+    return true
+  }
+  return false
+}
+
+const nbrValidPassports = (path: string, validateFields: boolean = false) => {
+  const requiredFieldIds = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
   const lines = fileReader.readStringArray(path, '\n\n')
   return lines
     .map((line) => line.replace(/(\r\n|\n|\r)/gm, ' '))
-    .map((line) => line.split(' ').map((row) => row.substr(0, 3)))
-    .map((password) =>
-      requiredFields.every((field) => password.includes(field))
+    .map((line) => line.split(' ').map((fieldString) => fieldString.split(':')))
+    .map((passport) =>
+      passport.map((field) =>
+        validateFields && !isFieldValid(field) ? [undefined, undefined] : field
+      )
+    )
+    .map((passport) => passport.map((field) => field[0]))
+    .map((fieldIds) =>
+      requiredFieldIds.every((fieldId) => fieldIds.includes(fieldId))
     )
     .reduce(
       (accumulator, currentValue) => accumulator + (currentValue ? 1 : 0),
@@ -15,4 +50,4 @@ const nbrValidPassports = (path: string) => {
     )
 }
 
-export default { nbrValidPassports }
+export default { nbrValidPassports, isFieldValid }
