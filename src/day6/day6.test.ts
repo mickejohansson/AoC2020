a2376e8c0b5784803b3b29495a8da745bb852899
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-06 10:07:27 +0100

    Solve part 2

--- src/day6/day6.test.ts
+++ src/day6/day6.test.ts
@@ -14,3 +14,35 @@ test('it counts the number of positive answers in the real input', () => {
     .map((line) => line.split('\n'))
   expect(day6.nbrYes(testInput)).toBe(6885)
 })
+
+test('it gets the intersection of multiple arrays', () => {
+  expect(
+    day6.intersection([
+      ['A', 'B', 'C'],
+      ['A', 'B']
+    ])
+  ).toMatchObject(['A', 'B'])
+
+  expect(day6.intersection([['A', 'B', 'C']])).toMatchObject(['A', 'B', 'C'])
+
+  expect(
+    day6.intersection([
+      ['A', 'B', 'C', 'D'],
+      ['B', 'D', 'E']
+    ])
+  ).toMatchObject(['B', 'D'])
+})
+
+test('it counts the number of answers that was positive for all persons in a group in the test input', () => {
+  const testInput = fileReader
+    .readStringArray(__dirname + '/testInput.txt', '\n\n')
+    .map((line) => line.split('\n'))
+  expect(day6.nbrAllYes(testInput)).toBe(6)
+})
+
+test('it counts the number of answers that was positive for all persons in a group in the real input', () => {
+  const testInput = fileReader
+    .readStringArray(__dirname + '/input.txt', '\n\n')
+    .map((line) => line.split('\n'))
+  expect(day6.nbrAllYes(testInput)).toBe(3550)
+})
--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -9,4 +9,16 @@ const nbrYes = (groups: Array<Array<string>>): number => {
     .reduce((acc, curr) => acc + curr.size, 0)
 }
 
-export default { nbrYes }
+const intersection = (arrs: Array<Array<string>>): Array<string> => {
+  return arrs.map((arr) =>
+    arr.filter((value) => arrs.every((a) => a.includes(value)))
+  )[0]
+}
+
+const nbrAllYes = (groups: Array<Array<string>>): number => {
+  return groups
+    .map((group) => group.map((answer) => answer.split('')))
+    .flatMap((group) => intersection(group)).length
+}
+
+export default { nbrYes, nbrAllYes, intersection }
