faa24d372d3b24919da5a99108369091cdb7760c
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-06 10:26:53 +0100

    Clean up

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,12 +1,7 @@
-const nbrYes = (groups: Array<Array<string>>): number => {
-  return groups
-    .map((group) => group.flatMap((line) => line.split('')))
-    .map((group) => {
-      const m = new Map()
-      group.forEach((answer) => m.set(answer, true))
-      return m
-    })
-    .reduce((acc, curr) => acc + curr.size, 0)
+const union = (arrs: Array<Array<string>>): Array<string> => {
+  const s = new Set<string>()
+  arrs.flat().forEach((value) => s.add(value))
+  return Array.from(s)
 }
 
 const intersection = (arrs: Array<Array<string>>): Array<string> => {
@@ -15,10 +10,16 @@ const intersection = (arrs: Array<Array<string>>): Array<string> => {
   )[0]
 }
 
+const nbrYes = (groups: Array<Array<string>>): number => {
+  return groups
+    .map((group) => group.map((line) => line.split('')))
+    .flatMap((group) => union(group)).length
+}
+
 const nbrAllYes = (groups: Array<Array<string>>): number => {
   return groups
     .map((group) => group.map((answer) => answer.split('')))
     .flatMap((group) => intersection(group)).length
 }
 
-export default { nbrYes, nbrAllYes, intersection }
+export default { union, intersection, nbrYes, nbrAllYes }
