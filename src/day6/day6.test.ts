938a5358354214ec6de0797fc85a741cc3197eee
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-06 09:03:01 +0100

    Solve part 1

--- src/day6/day6.test.ts
+++ src/day6/day6.test.ts
@@ -1,9 +1,16 @@
 import day6 from './day6'
 import fileReader from '../util/fileReader'
 
-test('it counts the number of yeses', () => {
+test('it counts the number of positive answers in the test input', () => {
   const testInput = fileReader
     .readStringArray(__dirname + '/testInput.txt', '\n\n')
     .map((line) => line.split('\n'))
   expect(day6.nbrYes(testInput)).toBe(11)
+})
+
+test('it counts the number of positive answers in the real input', () => {
+  const testInput = fileReader
+    .readStringArray(__dirname + '/input.txt', '\n\n')
+    .map((line) => line.split('\n'))
+  expect(day6.nbrYes(testInput)).toBe(6885)
 })
