84a91939dc6d9126c6ab346a204c1a761b175766
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-09 08:34:58 +0100

    Uncomment test

--- src/day9/day9.test.ts
+++ src/day9/day9.test.ts
@@ -1,11 +1,9 @@
 import fileReader from '../util/fileReader'
 import day9 from './day9'
 
-/*
 test('it checks if a number is the sum of numbers in an array', () => {
   expect(day9.isSumOf(5, [3, 2, 7, 9, 2])).toBe(true)
 })
-*/
 
 test('it finds the first erroneus number', () => {
   let nbrs = fileReader.readIntArray(__dirname + '/testInput.txt')
