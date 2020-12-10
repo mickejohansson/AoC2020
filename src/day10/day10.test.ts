44f6aa45fce31c555198d14fd6f8bbb7a7b158e4
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-10 22:52:22 +0100

    Solve part 2

--- src/day10/day10.test.ts
+++ src/day10/day10.test.ts
@@ -8,3 +8,17 @@ test('it connects all adapters', () => {
   joltages = fileReader.readIntArray(__dirname + '/input.txt')
   expect(day10.connectAdapters(joltages)).toMatchObject([0, 68, 0, 28])
 })
+
+test('it finds the number of ways to connect adapters', () => {
+  let joltages = fileReader.readIntArray(__dirname + '/testInput.txt')
+  expect(day10.getNbrCombinations(joltages)).toBe(8)
+
+  joltages = fileReader.readIntArray(__dirname + '/testInput2.txt')
+  expect(day10.getNbrCombinations(joltages)).toBe(19208)
+
+  joltages = fileReader.readIntArray(__dirname + '/testInput3.txt')
+  expect(day10.getNbrCombinations(joltages)).toBe(149)
+
+  joltages = fileReader.readIntArray(__dirname + '/input.txt')
+  expect(day10.getNbrCombinations(joltages)).toBe(10578455953408)
+})
--- src/day10/day10.ts
+++ src/day10/day10.ts
@@ -1,4 +1,4 @@
-const connectAdapters = (joltages: number[]) => {
+const connectAdapters = (joltages: number[]): number[] => {
   return joltages
     .sort((a, b) => a - b)
     .reduce(
@@ -11,4 +11,38 @@ const connectAdapters = (joltages: number[]) => {
     )
 }
 
-export default { connectAdapters }
+let combinations: Map<number, number> = new Map()
+const combinationsFor = (
+  joltage: number,
+  joltages: number[],
+  last: number
+): number => {
+  if (joltage === last) {
+    return 1
+  }
+
+  const cached = combinations.get(joltage)
+  if (cached !== undefined) {
+    return cached
+  }
+
+  const c = joltages
+    .filter((j) => j > joltage && j <= joltage + 3)
+    .map((j) => combinationsFor(j, joltages, last))
+    .reduce((acc, curr) => acc + curr, 0)
+
+  combinations.set(joltage, c)
+  return c
+}
+
+const getNbrCombinations = (joltages: number[]): number => {
+  joltages.push(0)
+  joltages = joltages.sort((a, b) => a - b)
+  combinations = new Map()
+  joltages.forEach((j, index, arr) =>
+    combinations.set(j, combinationsFor(j, arr, joltages[joltages.length - 1]))
+  )
+  return combinations.get(0)
+}
+
+export default { connectAdapters, getNbrCombinations }
