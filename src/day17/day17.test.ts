3be9dd376a8c860c0c385e15bba8dbc30edc3824
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-19 13:46:21 +0100

    Solve part 2

--- src/day17/day17.test.ts
+++ src/day17/day17.test.ts
@@ -2,20 +2,20 @@ import day17 from './day17'
 
 test('it parses the input', () => {
   const map = day17.parse(__dirname + '/testInput.txt')
-  expect(map.get(day17.hash(1, 0, 0)).active).toBe(true)
-  expect(map.get(day17.hash(2, 1, 0)).active).toBe(true)
-  expect(map.get(day17.hash(0, 2, 0)).active).toBe(true)
-  expect(map.get(day17.hash(1, 2, 0)).active).toBe(true)
-  expect(map.get(day17.hash(2, 2, 0)).active).toBe(true)
+  expect(map.get(day17.hash(1, 0, 0, 0)).active).toBe(true)
+  expect(map.get(day17.hash(2, 1, 0, 0)).active).toBe(true)
+  expect(map.get(day17.hash(0, 2, 0, 0)).active).toBe(true)
+  expect(map.get(day17.hash(1, 2, 0, 0)).active).toBe(true)
+  expect(map.get(day17.hash(2, 2, 0, 0)).active).toBe(true)
 
-  expect(map.get(day17.hash(0, 0, -1)).active).toBe(false)
-  expect(map.get(day17.hash(0, 0, -1)).activeNeighbours).toBe(1)
+  expect(map.get(day17.hash(0, 0, -1, 0)).active).toBe(false)
+  expect(map.get(day17.hash(0, 0, -1, 0)).activeNeighbours).toBe(1)
 
-  expect(map.get(day17.hash(1, 1, -1)).active).toBe(false)
-  expect(map.get(day17.hash(1, 1, -1)).activeNeighbours).toBe(5)
+  expect(map.get(day17.hash(1, 1, -1, 0)).active).toBe(false)
+  expect(map.get(day17.hash(1, 1, -1, 0)).activeNeighbours).toBe(5)
 
-  expect(map.get(day17.hash(2, 1, 1)).active).toBe(false)
-  expect(map.get(day17.hash(2, 1, 1)).activeNeighbours).toBe(4)
+  expect(map.get(day17.hash(2, 1, 1, 0)).active).toBe(false)
+  expect(map.get(day17.hash(2, 1, 1, 0)).activeNeighbours).toBe(4)
 })
 
 test('it runs 2 cycles on the test input', () => {
@@ -26,10 +26,26 @@ test('it runs 2 cycles on the test input', () => {
   expect(day17.nbrActive(map)).toBe(21)
 })
 
+test('it runs 2 cycles on the test input considering the fourth dimension', () => {
+  let map = day17.parse(__dirname + '/testInput.txt')
+  map = day17.runCycle(map, true)
+  expect(day17.nbrActive(map)).toBe(29)
+  map = day17.runCycle(map, true)
+  expect(day17.nbrActive(map)).toBe(60)
+})
+
 test('it runs 6 cycles on the real input', () => {
   let map = day17.parse(__dirname + '/input.txt')
   for (let i = 0; i < 6; i++) {
     map = day17.runCycle(map)
   }
   expect(day17.nbrActive(map)).toBe(353)
+})
+
+test('it runs 6 cycles on the real input considering the fourth dimension', () => {
+  let map = day17.parse(__dirname + '/input.txt')
+  for (let i = 0; i < 6; i++) {
+    map = day17.runCycle(map, true)
+  }
+  expect(day17.nbrActive(map)).toBe(2472)
 })
--- src/day17/day17.ts
+++ src/day17/day17.ts
@@ -4,59 +4,51 @@ interface Cube {
   x: number
   y: number
   z: number
+  w: number
 
   active: boolean
   activeNeighbours: number
 }
 
-const hash = (x: number, y: number, z: number): string => {
-  return x + ':' + y + ':' + z
-}
-
-const addEmptyIfMissing = (
-  x: number,
-  y: number,
-  z: number,
-  map: Map<string, Cube>
-) => {
-  const key = hash(x, y, z)
-  if (!map.has(key)) {
-    map.set(key, { x, y, z, active: false, activeNeighbours: 0 })
-  }
+const hash = (x: number, y: number, z: number, w: number): string => {
+  return x + ':' + y + ':' + z + ':' + w
 }
 
 const notifyNeighbours = (
   x: number,
   y: number,
   z: number,
+  w: number,
   turnedActive: boolean,
   map: Map<string, Cube>
 ) => {
-  //console.log('------ Checking for: x:' + x + ' y:' + y + ' z:' + z)
   for (let dX = -1; dX <= 1; dX++) {
     for (let dY = -1; dY <= 1; dY++) {
       for (let dZ = -1; dZ <= 1; dZ++) {
-        const newX = x + dX
-        const newY = y + dY
-        const newZ = z + dZ
-        if (!(dX === 0 && dY === 0 && dZ === 0)) {
-          //console.log('Checking: x:' + dX + ' y:' + dY + ' z:' + dZ)
-          const key = hash(newX, newY, newZ)
-          const cube = map.get(key)
-          if (turnedActive) {
-            if (!cube) {
-              map.set(key, {
-                x: newX,
-                y: newY,
-                z: newZ,
-                active: false,
-                activeNeighbours: 1
-              })
+        for (let dW = -1; dW <= 1; dW++) {
+          const newX = x + dX
+          const newY = y + dY
+          const newZ = z + dZ
+          const newW = w + dW
+          if (!(dX === 0 && dY === 0 && dZ === 0 && dW === 0)) {
+            const key = hash(newX, newY, newZ, newW)
+            const cube = map.get(key)
+            if (turnedActive) {
+              if (!cube) {
+                map.set(key, {
+                  x: newX,
+                  y: newY,
+                  z: newZ,
+                  w: newW,
+                  active: false,
+                  activeNeighbours: 1
+                })
+              } else {
+                cube.activeNeighbours++
+              }
             } else {
-              cube.activeNeighbours++
+              cube.activeNeighbours--
             }
-          } else {
-            cube.activeNeighbours--
           }
         }
       }
@@ -68,23 +60,24 @@ const update = (
   x: number,
   y: number,
   z: number,
+  w: number,
   active: boolean,
   map: Map<string, Cube>
 ) => {
-  const key = hash(x, y, z)
+  const key = hash(x, y, z, w)
   const existingCube = map.get(key)
   const wasActive = existingCube ? existingCube.active : false
   const activeNeighbours = existingCube ? existingCube.activeNeighbours : 0
 
   if (active && !wasActive) {
     // Cube has turned active
-    notifyNeighbours(x, y, z, true, map)
+    notifyNeighbours(x, y, z, w, true, map)
   } else if (wasActive && !active) {
     // Cube has turned inactive
-    notifyNeighbours(x, y, z, false, map)
+    notifyNeighbours(x, y, z, w, false, map)
   }
 
-  map.set(key, { x, y, z, active, activeNeighbours })
+  map.set(key, { x, y, z, w, active, activeNeighbours })
 }
 
 const parse = (path: string): Map<string, Cube> => {
@@ -93,12 +86,11 @@ const parse = (path: string): Map<string, Cube> => {
   fileReader.readStringArray(path).map((row, y) =>
     row.split('').forEach((char, x) => {
       if (char === '#') {
-        update(x, y, 0, true, map)
+        update(x, y, 0, 0, true, map)
       }
     })
   )
 
-  //console.log('Map', map)
   return map
 }
 
@@ -113,18 +105,42 @@ const nbrActive = (map: Map<string, Cube>): number => {
   return sum
 }
 
-const runCycle = (map: Map<string, Cube>): Map<string, Cube> => {
+const runCycle = (
+  map: Map<string, Cube>,
+  fourDimensions: boolean = false
+): Map<string, Cube> => {
   const newMap: Map<string, Cube> = new Map()
   map.forEach((cube, key) => {
     if (cube.active) {
       if (cube.activeNeighbours === 2 || cube.activeNeighbours === 3) {
-        update(cube.x, cube.y, cube.z, true, newMap)
+        update(
+          cube.x,
+          cube.y,
+          cube.z,
+          fourDimensions ? cube.w : 0,
+          true,
+          newMap
+        )
       } else {
-        update(cube.x, cube.y, cube.z, false, newMap)
+        update(
+          cube.x,
+          cube.y,
+          cube.z,
+          fourDimensions ? cube.w : 0,
+          false,
+          newMap
+        )
       }
     } else {
       if (cube.activeNeighbours === 3) {
-        update(cube.x, cube.y, cube.z, true, newMap)
+        update(
+          cube.x,
+          cube.y,
+          cube.z,
+          fourDimensions ? cube.w : 0,
+          true,
+          newMap
+        )
       } else {
         // Remain inactive
       }
