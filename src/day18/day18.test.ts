41bee49be91b923be451f271736f9b2b38300b0b
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-20 09:08:43 +0100

    Solve part 1

--- src/day18/day18.test.ts
+++ src/day18/day18.test.ts
@@ -1,5 +1,24 @@
 import day18 from './day18'
+import fileReader from '../util/fileReader'
 
 test('it evaulates mathematical expressions', () => {
+  expect(day18.evaluate('1 + 2 * 3 + 4 * 5 + 6')).toBe(71)
   expect(day18.evaluate('1 + (2 * 3) + (4 * (5 + 6))')).toBe(51)
+  expect(day18.evaluate('2 * 3 + (4 * 5)')).toBe(26)
+  expect(day18.evaluate('5 + (8 * 3 + 9 + 3 * 4 * 3)')).toBe(437)
+  expect(day18.evaluate('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')).toBe(
+    12240
+  )
+  expect(
+    day18.evaluate('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')
+  ).toBe(13632)
+})
+
+test('it sums up all solutions', () => {
+  const sum = fileReader
+    .readStringArray(__dirname + '/input.txt')
+    .map((line) => day18.evaluate(line))
+    .reduce((acc, curr) => acc + curr, 0)
+
+  expect(sum).toBe(53660285675207)
 })
--- src/day18/day18.ts
+++ src/day18/day18.ts
@@ -1,5 +1,48 @@
+const operate = (left: number, operator: string, right: number): number => {
+  if (operator === '+') {
+    return left + right
+  } else {
+    return left * right
+  }
+}
+
 const evaluate = (expression: string): number => {
-  return undefined
+  let result = 0
+  let operator = '+'
+  let i = 0
+  while (i < expression.length) {
+    const char = expression.charAt(i)
+    const nbr = parseInt(char)
+    if (!isNaN(nbr)) {
+      result = operate(result, operator, nbr)
+    } else if (char === '+' || char === '*') {
+      operator = char
+    } else if (char === '(') {
+      let depth = 1
+      let j = i + 1
+      let found = false
+      while (j < expression.length && !found) {
+        if (expression.charAt(j) === '(') {
+          depth++
+        } else if (expression.charAt(j) === ')') {
+          depth--
+          if (depth === 0) {
+            found = true
+            break
+          }
+        }
+        j++
+      }
+      result = operate(
+        result,
+        operator,
+        evaluate(expression.substring(i + 1, j))
+      )
+      i = j
+    }
+    i++
+  }
+  return result
 }
 
 export default { evaluate }
