938438d69c667841d4af5ed8b7b09495fe29058a
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-20 16:15:26 +0100

    First approach

--- src/day19/day19.test.ts
+++ src/day19/day19.test.ts
@@ -1,9 +1,21 @@
 import day19 from './day19'
 import fileReader from '../util/fileReader'
 
-test('it evaulates mathematical expressions', () => {
+test('it finds the number of matching messages in the test data', () => {
   const input = fileReader
     .readStringArray(__dirname + '/testInput.txt', '\n\n')
+    .map((chunk) => chunk.split('\n'))
+  expect(
+    day19.nbrMatching(
+      input[0].map((rule) => rule.split(': ')[1]),
+      input[1]
+    )
+  ).toBe(2)
+})
+
+test('it finds the number of matching messages in the real data', () => {
+  const input = fileReader
+    .readStringArray(__dirname + '/input.txt', '\n\n')
     .map((chunk) => chunk.split('\n'))
   expect(
     day19.nbrMatching(
--- src/day19/day19.ts
+++ src/day19/day19.ts
@@ -1,7 +1,30 @@
+const ruleRegex = (rule: string, rules: string[]): string => {
+  return (
+    (rule.includes('|') ? '(' : '') +
+    rule
+      .split(' | ')
+      .map((altRule) =>
+        altRule
+          .split(' ')
+          .map((rulePart) => {
+            if (rulePart.startsWith('"')) {
+              return rulePart.charAt(1)
+            } else {
+              return ruleRegex(rules[parseInt(rulePart)], rules)
+            }
+          })
+          .join('')
+      )
+      .join('|') +
+    (rule.includes('|') ? ')' : '')
+  )
+}
+
 const nbrMatching = (rules: string[], messages: string[]): number => {
-  console.log('rules', rules)
-  console.log('messages', messages)
-  return undefined
+  const regex = ruleRegex(rules[0], rules)
+  return messages
+    .map((message) => message.match('^' + regex + '$'))
+    .reduce((acc, curr) => acc + (curr ? 1 : 0), 0)
 }
 
 export default { nbrMatching }
