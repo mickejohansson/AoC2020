0069a1382dd47126c53fc58d74aa36820e69940e
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-23 19:14:48 +0100

    Solving for the test data

--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -36,8 +36,52 @@ const play = (path: string): number => {
   return score(decks[0].length === 0 ? decks[1] : decks[0])
 }
 
-const playRecursiveGame = (startDecks: number[][]): number => {
-  return undefined
+const playRecursive = (decks: number[][]): number => {
+  const previousRoundDecks: string[] = []
+
+  //console.log('New game', decks)
+
+  let i = 1
+  while (decks[0].length > 0 && decks[1].length > 0) {
+    // If these hands have been played in a previous round, player 1 wins the game
+    const round = decks[0].join(',') + ':' + decks[1].join(',')
+    if (previousRoundDecks.includes(round)) {
+      return 1
+    } else {
+      previousRoundDecks.push(round)
+    }
+
+    let winner = 0
+
+    // If both players have at least as many cards left as the card they drew, recurse
+    if (decks[0][0] < decks[0].length && decks[1][0] < decks[1].length) {
+      winner = playRecursive(decks.map((deck) => deck.slice(1, deck[0] + 1)))
+    } else if (decks[1][0] > decks[0][0]) {
+      winner = 1
+    }
+
+    const loser = (winner + 1) % 2
+
+    //    console.log('Round ' + i + ' winner ' + (winner + 1), decks)
+
+    decks[winner].push(decks[winner][0])
+    decks[winner].push(decks[loser][0])
+
+    decks[winner] = decks[winner].slice(1)
+    decks[loser] = decks[loser].slice(1)
+
+    i++
+  }
+
+  return decks[0].length === 0 ? 1 : 0
+}
+
+const playRecursiveGame = (decks: number[][]): number => {
+  const winner = playRecursive(decks)
+
+  console.log('Post game', decks)
+
+  return score(decks[winner])
 }
 
 export default { play, startDecks, playRecursiveGame }
