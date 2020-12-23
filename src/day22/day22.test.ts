89bac7430ffe26cbd91121fe0bc80c85058eafb6
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-23 15:41:21 +0100

    Solve part 1

--- src/day22/day22.test.ts
+++ src/day22/day22.test.ts
@@ -1,5 +1,6 @@
 import day22 from './day22'
 
 test('it plays a game and returns the winning result', () => {
-  expect(day22.play(__dirname + '/testInput.txt')).toBe(5)
+  expect(day22.play(__dirname + '/testInput.txt')).toBe(306)
+  expect(day22.play(__dirname + '/input.txt')).toBe(33400)
 })
--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -9,11 +9,31 @@ const startDecks = (path: string): number[][] => {
   )
 }
 
+const score = (cards: number[]): number => {
+  return cards
+    .reverse()
+    .reduce((acc, curr, index) => acc + curr * (index + 1), 0)
+}
+
 const play = (path: string): number => {
   const decks = startDecks(path)
-  console.log('Start Decks', decks)
 
-  return undefined
+  while (decks[0].length > 0 && decks[1].length > 0) {
+    let winner = 0
+    let loser = 1
+    if (decks[1][0] > decks[0][0]) {
+      winner = 1
+      loser = 0
+    }
+
+    decks[winner].push(decks[winner][0])
+    decks[winner].push(decks[loser][0])
+
+    decks[winner] = decks[winner].slice(1)
+    decks[loser] = decks[loser].slice(1)
+  }
+
+  return score(decks[0].length === 0 ? decks[1] : decks[0])
 }
 
 export default { play }
