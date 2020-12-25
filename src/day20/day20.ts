0ad1076192e72ebfa83b56e5d5c0778f84e01888
Mikael Johansson <mikael@mikaeljohansson.org>
2020-12-26 00:37:17 +0100

    Cleanup

--- src/day20/day20.ts
+++ src/day20/day20.ts
@@ -116,7 +116,6 @@ const matchTile = (topBorder: number, leftBorder: number, tiles: Tile[], sideBor
     for(let r=0; r<4; r++) {
       tile = rotateCW(tile)
       const borders = getBorders(tile, sideBorders)
-      //console.log('Checking border for ' + tile.id, borders)
       if (borders[Side.TOP] === topBorder && borders[Side.LEFT] === leftBorder) {
         return {
           tile,
@@ -128,7 +127,6 @@ const matchTile = (topBorder: number, leftBorder: number, tiles: Tile[], sideBor
     for(let r=0; r<4; r++) {
       tile = rotateCW(tile)
       const borders = getBorders(tile, sideBorders)
-      //console.log('Checking inv border for ' + tile.id, borders)
       if (borders[Side.TOP] === topBorder && borders[Side.LEFT] === leftBorder) {
         return {
           tile,
@@ -138,7 +136,6 @@ const matchTile = (topBorder: number, leftBorder: number, tiles: Tile[], sideBor
     }
   }
 
-  console.log('Failed finding match for top: ' + topBorder + ' left: ' + leftBorder)
   return undefined
 }
 
@@ -176,11 +173,7 @@ const removeTile = (tile: Tile, tiles: Tile[]): Tile[] => {
 const buildMap = (path: string): MatchedTile[][] => {
   let tiles = parseTiles(path)
 
-  //const corners = findCorners(tiles)
-  //console.log('corners', corners)
-
   const sides = getSides(tiles)
-  console.log('sides', sides)
 
 
   const width = Math.sqrt(tiles.length)
@@ -188,19 +181,14 @@ const buildMap = (path: string): MatchedTile[][] => {
   for(let y = 0; y < width; y++) {
     const row = []
     for(let x = 0; x < width; x++) { 
-      //console.log('checking ' + x + ':' + y)
       const left = x > 0 ? row[x-1].borders[Side.RIGHT] : -1
       const top = y > 0 ? map[y-1][x].borders[Side.BOTTOM] : -1
       const matchedTile = matchTile(top, left, tiles, sides)
-      //console.log('Fit for left: ' + left + ' top: ' + top, matchedTile)
       tiles = removeTile(matchedTile.tile, tiles)
-      //console.log('length', tiles.length)
       row.push(matchedTile)
     }
     map.push(row)
   }
-  console.log('map', map)
-
 
   return map
 }
@@ -225,7 +213,6 @@ const getImage = (path: string): string[] => {
 
   // TODO fix!
   image = image.slice(1)
-  console.log('image', image)
 
   return image
 }
@@ -242,7 +229,6 @@ const findAllMonsters = (regex: RegExp, image: string): number => {
   let match = regex.exec(image)
   let nbrMatches = 0
   while (match) {
-    console.log('match', match)
     nbrMatches++
     regex.lastIndex = match.index + 1
     match = regex.exec(image)
@@ -255,7 +241,6 @@ const findMonsters = (image: string[]): number => {
   for(let i=0; i<4; i++) {
     image = rotateImage(image)
     let matches = image.join('').match(regex)
-    console.log('matches', matches)
     if (matches) {
       return findAllMonsters(regex, image.join(''))
     }
@@ -264,7 +249,6 @@ const findMonsters = (image: string[]): number => {
   for(let i=0; i<4; i++) {
     image = rotateImage(image)
     let matches = image.join('').match(regex)
-    console.log('matches', matches)
     if (matches) {
       return findAllMonsters(regex, image.join(''))
     }
@@ -275,127 +259,7 @@ const findMonsters = (image: string[]): number => {
 
 const roughness = (image: string[]): number => {
   const nbrMatches = findMonsters(image)
-  console.log('nbrmatches', nbrMatches)
   return image.flatMap(line => line.split('')).filter(s => s === '#').length - nbrMatches * 15
 }
 
 export default { parseTiles, rotateCW, flipHorizontal, getBorders, buildMap, cornerProduct, getImage, roughness }
-
-/*
-const fromBinary = (bin: string[]): number => {
-  return parseInt(bin.join(''), 2)
-}
-
-const getBorders = (tileData: string[][]): number[] => {
-  const bin: string[][] = tileData.map((row) =>
-    row.map((c) => (c === '#' ? '1' : '0'))
-  )
-
-  return [
-    fromBinary(bin[0]), // TOP
-    fromBinary(bin.map((row) => row[row.length - 1])), // RIGHT
-    fromBinary(bin[bin.length - 1].map((c) => c).reverse()), // BOTTOM
-    fromBinary(
-      bin
-        .map((row) => row[0])
-        .map((c) => c)
-        .reverse()
-    ) // LEFT
-  ]
-}
-
-const parseTiles = (path: string): Tile[] => {
-  return fileReader
-    .readStringArray(path, '\n\n')
-    .map((chunk) => chunk.split('\n'))
-    .map((tileInfo) => {
-      const id = parseInt(tileInfo[0].match(/Tile (\d+):/)[1])
-      const data = tileInfo.slice(1).map((row) => row.split(''))
-      const borders = getBorders(data)
-      return {
-        id,
-        data,
-        borders
-      }
-    })
-}
-
-const rotateCW = (tile: Tile) => {
-  const borders = Array.from(tile.borders)
-  for (let i = 0; i < borders.length; i++) {
-    borders[i] = tile.borders[(i - 1 + 4) % 4]
-  }
-
-  tile.borders = borders
-}
-
-// Flip left to right
-const flip = (tile: Tile) => {
-  const reversedData = tile.data.map((row) => row.reverse())
-  const borders = getBorders(reversedData)
-  tile.borders = borders
-}
-
-const testTile = (tile: Tile, side: Side, currentTile: Tile): Tile => {
-  for (let b = 0; b < currentTile.borders.length; b++) {
-    if (currentTile.borders[(side + 2) % 4] === tile.borders[side]) {
-      return currentTile
-    }
-    rotateCW(currentTile)
-  }
-}
-
-const findMatchingTile = (tile: Tile, side: Side, tiles: Tile[]): Tile => {
-  for (let i = 0; i < tiles.length; i++) {
-    const currentTile = {
-      id: tiles[i].id,
-      data: Array.from(tiles[i].data.map((row) => Array.from(row))),
-      borders: Array.from(tiles[i].borders)
-    }
-    if (currentTile.id !== tile.id) {
-      let foundTile = testTile(tile, side, currentTile)
-      if (!foundTile) {
-        flip(currentTile)
-        foundTile = testTile(tile, side, currentTile)
-      }
-      if (foundTile) {
-        return foundTile
-      }
-    }
-  }
-}
-
-const findNeighbours = (tile: Tile, tiles: Tile[]): Tile[] => {
-  return [
-    findMatchingTile(tile, Side.TOP, tiles),
-    findMatchingTile(tile, Side.RIGHT, tiles),
-    findMatchingTile(tile, Side.BOTTOM, tiles),
-    findMatchingTile(tile, Side.LEFT, tiles)
-  ]
-}
-
-const cornerProduct = (path: string): number => {
-  const tiles = parseTiles(path)
-  return tiles
-    .filter(
-      (tile) =>
-        findNeighbours(tile, tiles).filter((n) => n !== undefined).length === 2
-    )
-    .reduce((acc, curr) => acc * curr.id, 1)
-}
-
-const buildMap = (path: string): Tile[][] => {
-  const tiles = parseTiles(path)
-
-  return []
-}
-
-export default {
-  parseTiles,
-  buildMap,
-  rotateCW,
-  flip,
-  findMatchingTile,
-  cornerProduct
-}
-*/
